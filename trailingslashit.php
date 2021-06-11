30a2d58d6e9c7c8b52143136375843d1286bb99f
Fred Bradley <code@fredbradley.co.uk>
2021-06-11 13:46:22 +0100

    if statement so as not to conflict otherwise

--- trailingslashit.php
+++ trailingslashit.php
@@ -1,32 +1,36 @@
 <?php
 
-/**
- * Will remove trailing forward and backslashes if it exists already before adding
- * a trailing forward slash. This prevents double slashing a string or path.
- *
- * The primary use of this is for paths and thus should be used for paths. It is
- * not restricted to paths and offers no specific path support.
- *
- * @param  string  $string What to add the trailing slash to.
- *
- * @return string String with the trailing slash added.
- */
-function trailingslashit(string $string): string
-{
-    return untrailingslashit($string).'/';
+if (!function_exists('trailingslashit')) {
+    /**
+     * Will remove trailing forward and backslashes if it exists already before adding
+     * a trailing forward slash. This prevents double slashing a string or path.
+     *
+     * The primary use of this is for paths and thus should be used for paths. It is
+     * not restricted to paths and offers no specific path support.
+     *
+     * @param  string  $string  What to add the trailing slash to.
+     *
+     * @return string String with the trailing slash added.
+     */
+    function trailingslashit(string $string): string
+    {
+        return untrailingslashit($string).'/';
+    }
 }
 
-/**
- * Removes trailing forward slashes and backslashes if they exist.
- *
- * The primary use of this is for paths and thus should be used for paths. It is
- * not restricted to paths and offers no specific path support.
- *
- * @param  string  $string What to remove the trailing slashes from.
- *
- * @return string String without the trailing slashes.
- */
-function untrailingslashit(string $string): string
-{
-    return rtrim($string, '/\\');
+if (!function_exists('untrailingslashit')) {
+    /**
+     * Removes trailing forward slashes and backslashes if they exist.
+     *
+     * The primary use of this is for paths and thus should be used for paths. It is
+     * not restricted to paths and offers no specific path support.
+     *
+     * @param  string  $string  What to remove the trailing slashes from.
+     *
+     * @return string String without the trailing slashes.
+     */
+    function untrailingslashit(string $string): string
+    {
+        return rtrim($string, '/\\');
+    }
 }
